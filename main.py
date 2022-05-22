36006844da2c7d9ad3a323c8deee56abb1727ac2
danniel-me <daniel.pacheco@jalasoft.com>
2022-05-22 11:47:39 -0400

    Updating cripting files

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ def criptText(text):
     print('Text to encript: ' + text)
     finalText = ''
     for letter in text:
-        finalText +=letter + 'x'
+        ascii = ord(letter)
+        ascii += 1
+        finalText += chr(ascii)
     return finalText
     
 def decriptText(text):
     print('Text to decript: ' + text)
     finalText = ''
-    counter = 0
     for letter in text:
-        if counter %2 == 0:
-            finalText += letter
-        counter += 1
+        ascii = ord(letter)
+        ascii -= 1
+        finalText += chr(ascii)
     return finalText
 
 def criptFile():
@@ -24,6 +25,15 @@ def criptFile():
     file.write(cripedtText)
     file.close()
     
+def decriptFile():
+    file = open('text.txt','r')
+    text = file.read()
+    file.close()
+    cripedtText = decriptText(text)
+    file = open('text.txt', 'w')
+    file.write(cripedtText)
+    file.close()
+    
 #criptFile()
 
 #result = decriptText('Txexxxtxox xpxrxuxexbxax xpxaxrxax xexnxcxrxixpxtxaxrx')
@@ -31,4 +41,4 @@ def criptFile():
 #file.write(result)
 #file.close()
 
-criptFile()
+decriptFile()
