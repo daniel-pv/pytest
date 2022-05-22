28eacbdc28a668df9e2a7ee4cf6c8ef33b2d5340
danniel-me <daniel.pacheco@jalasoft.com>
2022-05-22 11:10:23 -0400

    Adding txt read

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-def mayor(edad = 20):
-    if edad >= 18:
-        print('mayor')
-    else:
-        print('menor')
-        
-mayor(15)
+def criptText(text):
+    print('Text to encript: ' + text)
+    finalText = ''
+    for letter in text:
+        finalText +=letter + 'x'
+    return finalText
+    
+def decriptText(text):
+    print('Text to decript: ' + text)
+    finalText = ''
+    counter = 0
+    for letter in text:
+        if counter %2 == 0:
+            finalText += letter
+        counter += 1
+    return finalText
+
+def criptFile():
+    file = open('text.txt','r')
+    text = file.read()
+    file.close()
+    cripedtText = criptText(text)
+    file = open('text.txt', 'w')
+    file.write(cripedtText)
+    file.close()
+    
+#criptFile()
+
+#result = decriptText('Txexxxtxox xpxrxuxexbxax xpxaxrxax xexnxcxrxixpxtxaxrx')
+#file = open('text.txt', 'w')
+#file.write(result)
+#file.close()
+
+criptFile()
