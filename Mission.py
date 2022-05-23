7e4a00917bef1b3d56cea211e7a442beca6bbc31
danniel-me <daniel.pacheco@jalasoft.com>
2022-05-23 11:52:32 -0400

    Adding new mission

--- Mission.py
+++ Mission.py
@@ -1,8 +1,38 @@
 
+from os import major
 from typing import Iterable
 
 class Mission:
     
+    ## MAX DIGIT
+    # You have a number and you need to determine which digit in this number is the biggest.
+    #Input: A positive int.
+    #Output: An Int (0-9).
+    #Example:
+    #
+    #max_digit(0) == 0
+    #max_digit(52) == 5
+    #max_digit(634) == 6
+    #max_digit(1) == 1
+    #max_digit(10000) == 1
+    def max_digit(number: int) -> int:
+        numbers = str(number)
+        major = 0
+        for num in str(number):
+            if int(num) > major or num == '9':
+                major = int(num) 
+            else:
+                major = major
+        return major
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    # assert max_digit(0) == 0
+    # assert max_digit(52) == 5
+    # assert max_digit(634) == 6
+    # assert max_digit(1) == 1
+    # assert max_digit(10000) == 1
+    
+    ## REPLACE FIRST
     def replace_first(items: list) -> Iterable:
         if len(items) > 1:
             first = items[0]
